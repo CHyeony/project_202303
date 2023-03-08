d4d4fedca8ac72c15e0b4a41a2e2005e2bc466f2
youngxpepp <youngxpepp@gmail.com>
2023-03-08 21:27:48 +0900

    feat: JPA 기본 설정 및 엔티티 추가

---
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserAccount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_account_id")
+	private Long id;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "bio")
+	private String bio;
+
+	@Column(name = "image")
+	private String image;
+
+	@Builder
+	public UserAccount(Long id, String email, String password, String username, String bio, String image) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.username = username;
+		this.bio = bio;
+		this.image = image;
+	}
+}
