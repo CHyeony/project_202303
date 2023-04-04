f7b700e30e871c046353030444118175e61b35d1
youngxpepp <youngxpepp@gmail.com>
2023-04-04 21:32:39 +0900

    feat: 사용자 수정 API

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.auth.TokenParser;
+import com.example.demo.dto.UpdateUserRequest;
 import com.example.demo.dto.UserDto;
 import com.example.demo.service.UserService;
 
@@ -29,6 +32,18 @@ public class UserController {
 		UserDto userResponse = userService.getUser(userId);
 		Map<String, Object> responseBody = new HashMap<>();
 		responseBody.put("user", userResponse);
+		return responseBody;
+	}
+
+	@PutMapping
+	public Map<String, Object> updateUser(
+		@RequestHeader("Authorization") String token,
+		@RequestBody UpdateUserRequest request
+	) {
+		long userId = tokenParser.parseToken(token);
+		UserDto userDto = userService.update(userId, request.getUser());
+		Map<String, Object> responseBody = new HashMap<>();
+		responseBody.put("user", userDto);
 		return responseBody;
 	}
 }
