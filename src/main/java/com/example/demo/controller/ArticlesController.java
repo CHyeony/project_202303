ce03fb9fd06c5633616e053341fc710f310709dd
CHYeony <yeony5665@naver.com>
2023-05-14 17:09:10 +0900

    게시글 보기_수정

--- src/main/java/com/example/demo/controller/ArticlesController.java
+++ src/main/java/com/example/demo/controller/ArticlesController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,17 +11,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.auth.TokenParser;
 import com.example.demo.dto.ArticleDto;
 import com.example.demo.dto.ArticleRequest;
 import com.example.demo.service.ArticleService;
+
 import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/articles")
 @RequiredArgsConstructor
 public class ArticlesController {
+
 	private final TokenParser tokenParser;
+
 	private final ArticleService articleService;
+
 	@PostMapping
 	public Map<String, Object> createArticle(@RequestBody ArticleRequest articleRequest, @RequestHeader("Authorization") String token) {
 		long userId = tokenParser.parseToken(token);
@@ -29,6 +36,7 @@ public class ArticlesController {
 		responseBody.put("article", articleDto);
 		return responseBody;
 	}
+
 	@PutMapping("/{slug}")
 	public Map<String, Object> updateArtcile(
 			@PathVariable String slug,
