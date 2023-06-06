7d5575d064c7be937f0f694b707e9e3defe015ee
youngxpepp <youngxpepp@gmail.com>
2023-06-06 21:55:15 +0900

    feat: 사용자 프로필 조회 api 버그 수정

--- src/main/java/com/example/demo/controller/UserProfileController.java
+++ src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,42 +1,50 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.auth.TokenParser;
 import com.example.demo.dto.UserProfileDTO;
-import com.example.demo.entity.UserAccount;
 import com.example.demo.service.UserProfileService;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/profiles")
 @RequiredArgsConstructor
 public class UserProfileController {
 
-    private final UserProfileService userProfileService;
+	private final UserProfileService userProfileService;
 
-    private final TokenParser tokenParser;
+	private final TokenParser tokenParser;
 
-    // Authentication optional ========
-    @GetMapping("/{username}")
-    public UserProfileDTO getUserProfile(
-            @PathVariable("username") String username,
-         @RequestHeader(value = "Authorization", required = false) String token) {
+	// Authentication optional ========
+	@GetMapping("/{username}")
+	public UserProfileDTO.ProfileResponse getUserProfile(
+		@PathVariable("username") String username,
+		@RequestHeader(value = "Authorization", required = false) String token) {
 
-        return userProfileService.getProfileByUsername(username);
-    }
+		if (token == null) {
+			UserProfileDTO userProfileDTO = userProfileService.getProfileByUsername(username);
+            return new UserProfileDTO.ProfileResponse(userProfileDTO);
+		} else {
+			long userId = tokenParser.parseToken(token);
+			UserProfileDTO userProfileDTO = userProfileService.getProfileByUsername(userId, username);
+            return new UserProfileDTO.ProfileResponse(userProfileDTO);
+		}
+	}
 
-    @PostMapping("/{username}/follow")
-    public UserProfileDTO.ProfileResponse follow(
-        @PathVariable("username") String username,
-        @RequestHeader(value = "Authorization") String token
-    ) {
-        long userId = tokenParser.parseToken(token);
-        UserProfileDTO userProfileDTO = userProfileService.follow(userId, username);
-        return new UserProfileDTO.ProfileResponse(userProfileDTO);
-    }
+	@PostMapping("/{username}/follow")
+	public UserProfileDTO.ProfileResponse follow(
+		@PathVariable("username") String username,
+		@RequestHeader(value = "Authorization") String token
+	) {
+		long userId = tokenParser.parseToken(token);
+		UserProfileDTO userProfileDTO = userProfileService.follow(userId, username);
+		return new UserProfileDTO.ProfileResponse(userProfileDTO);
+	}
 }
